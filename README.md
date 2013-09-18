a5731f06891971b57d8800b6b0534f7196eb2922
Graham Ollis <perl@wdlabs.com>
2013-09-18 10:22:45 -0400

    doco

--- README.md
+++ README.md
@@ -2,6 +2,32 @@
 
 Expand or unexpand tabs in your dist
 
+# SYNOPSIS
+
+    [TextTabs]
+    tabstop   = 8
+    unexapand = 0
+
+# DESCRIPTION
+
+This [Dist::Zilla](http://search.cpan.org/perldoc?Dist::Zilla) plugin expands or unexpands tabs using [Text::Tab](http://search.cpan.org/perldoc?Text::Tab).
+
+# ATTRIBUTES
+
+## tabstop
+
+The length of the tabstop in characters.  This is usually 8, but some people prefer 4 or 2.
+
+## unexpand
+
+if set to true, then an unexpand is used on all the targeted files, that is spaces of the
+right length are converted into an equivalent number of tabs.  The default is false, or
+expand mode.
+
+# SEE ALSO
+
+[Text::Tabs](http://search.cpan.org/perldoc?Text::Tabs)
+
 # AUTHOR
 
 Graham Ollis <plicease@cpan.org>
--- lib/Dist/Zilla/Plugin/TextTabs.pm
+++ lib/Dist/Zilla/Plugin/TextTabs.pm
@@ -7,6 +7,34 @@ use Text::Tabs qw( expand unexpand );
 # ABSTRACT: Expand or unexpand tabs in your dist
 # VERSION
 
+=head1 SYNOPSIS
+
+ [TextTabs]
+ tabstop   = 8
+ unexapand = 0
+
+=head1 DESCRIPTION
+
+This L<Dist::Zilla> plugin expands or unexpands tabs using L<Text::Tab>.
+
+=head1 ATTRIBUTES
+
+=head2 tabstop
+
+The length of the tabstop in characters.  This is usually 8, but some people prefer 4 or 2.
+
+=head2 unexpand
+
+if set to true, then an unexpand is used on all the targeted files, that is spaces of the
+right length are converted into an equivalent number of tabs.  The default is false, or
+expand mode.
+
+=head1 SEE ALSO
+
+L<Text::Tabs>
+
+=cut
+
 with 'Dist::Zilla::Role::FileMunger',
      'Dist::Zilla::Role::FileFinderUser' => {
        default_finders => [ ':InstallModules', ':ExecFiles' ],
